34c45ea3bda56b608788cfed1f681333d25c0f85
lorainesinadjan <s2020285@usls.edu.ph>
2023-01-03 11:33:48 +0800

    Initial Commit

--- app/src/main/java/com/example/forevergems/Custom.java
+++ app/src/main/java/com/example/forevergems/Custom.java
@@ -12,8 +12,7 @@ import android.os.Bundle;
 public class Custom extends AppCompatActivity {
     private EditText basePriceInput;
     private EditText materialFactorInput;
-    private EditText colorFactorInput;
-    private EditText clarityFactorInput;
+    private EditText quantityFactorInput;
     private TextView priceOutput;
 
     @Override
@@ -22,8 +21,7 @@ public class Custom extends AppCompatActivity {
         setContentView(R.layout.activity_customize);
         basePriceInput = findViewById(R.id.base_price_input);
         materialFactorInput = findViewById(R.id.material_factor_input);
-        colorFactorInput = findViewById(R.id.color_factor_input);
-        clarityFactorInput = findViewById(R.id.clarity_factor_input);
+        quantityFactorInput = findViewById(R.id.color_factor_input);
         priceOutput = findViewById(R.id.price_output);
         Button calculateButton = findViewById(R.id.calculate_button);
         calculateButton.setOnClickListener(new View.OnClickListener() {
@@ -31,10 +29,9 @@ public class Custom extends AppCompatActivity {
             public void onClick(View view) {
                 double basePrice = Double.parseDouble(basePriceInput.getText().toString());
                 double materialFactor = Double.parseDouble(materialFactorInput.getText().toString());
-                double colorFactor = Double.parseDouble(colorFactorInput.getText().toString());
-                double clarityFactor = Double.parseDouble(clarityFactorInput.getText().toString());
-                double price = basePrice * materialFactor * colorFactor * clarityFactor;
-                priceOutput.setText(String.format("Price: $%.2f", price));
+                double quantityFactor = Double.parseDouble(quantityFactorInput.getText().toString());
+                double price = basePrice * materialFactor * quantityFactor;
+                priceOutput.setText(String.format("Price: ₱%.2f", price));
             }
         });
         Button resetButton = findViewById(R.id.reset_button);
@@ -43,9 +40,8 @@ public class Custom extends AppCompatActivity {
             public void onClick(View view) {
                 basePriceInput.setText("");
                 materialFactorInput.setText("");
-                colorFactorInput.setText("");
-                clarityFactorInput.setText("");
-                priceOutput.setText("Price: $0.00");
+                quantityFactorInput.setText("");
+                priceOutput.setText("Price: ₱0.00");
             }
         });
     }
